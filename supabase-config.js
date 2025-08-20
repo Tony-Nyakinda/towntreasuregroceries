807da8d685b23d39ce42c51efabd2406c52a826b
Tony Nyakinda <onderegerald@gmail.com>
2025-08-20 20:33:02 +0300

    Add dependencies for Netlify function

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,8 @@
 // supabase-config.js
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabaseUrl = 'https://toviekzgoxwumanyxkvv.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvdmlla3pnb3h3dW1hbnl4a3Z2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1MzIxNzUsImV4cCI6MjA3MTEwODE3NX0.eDgM2Bu7UsL3YMdFqVNruNCyiJvqsao44Noba1LfjdY';
 
+// Create and export the Supabase client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
